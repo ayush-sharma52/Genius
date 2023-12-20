26967e2f6688df582b2d036977f1cb263d344f40
ayush-sharma52 <ayush052sharma@gmail.com>
2023-12-20 23:09:16 +0530

    navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,21 @@
 import { UserButton } from "@clerk/nextjs";
 
-import MobileSideBar from "./mobile-sidebar";
+import MobileSidebar from "@/components/mobile-sidebar";
 import { getApiLimitCount } from "@/lib/api-limits";
 import { checkSubscription } from "@/lib/subscription";
 
-const Navbar=async()=>{
-    const apiLimitCount=getApiLimitCount();
-    const isPro = await checkSubscription();
-    return (
-        <div className="flex items-center p-4 ">
-            <MobileSideBar isPro={isPro} apiLimitCount={apiLimitCount}/>
-            <div className="flex w-full justify-end">
-            <UserButton afterSignOutUrl="/"/>
-            </div>
-        </div>
-    );
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+  const isPro = await checkSubscription();
+
+  return ( 
+    <div className="flex items-center p-4">
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+   );
 }
+ 
 export default Navbar;
